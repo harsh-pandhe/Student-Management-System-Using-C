b30fd2b624724dcff7a5c262fbfe2a635c4908ca
Harsh Pandhe <harshpandhehome@gmail.com>
2024-11-29 17:56:32 +0530

    Update 02

--- main.c
+++ main.c
@@ -2,83 +2,117 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 100
+
 struct Student
 {
     int roll;
-    char name[100];
-    char dept[100];
-    char course[100];
-    char year[100];
-    char semester[100];
-    char email[100];
-    char phone[100];
+    char name[MAX];
+    char dept[MAX];
+    char course[MAX];
+    char year[MAX];
+    char semester[MAX];
+    char email[MAX];
+    char phone[MAX];
+    struct Student *next;
 };
 
-int display(struct Student *s)
+void display(struct Student *head)
 {
-    printf("Student Information\n");
-    printf("Roll: %d\n", s->roll);
-    printf("Name: %s\n", s->name);
-    printf("Department: %s\n", s->dept);
-    printf("Course: %s\n", s->course);
-    printf("Year: %s\n", s->year);
-    printf("Semester: %s\n", s->semester);
-    printf("Email: %s\n", s->email);
-    printf("Phone: %s\n", s->phone);
+    struct Student *temp = head;
+    while (temp != NULL)
+    {
+        printf("Student Details: \n");
+        printf("Roll: %d\n", temp->roll);
+        printf("Name: %s\n", temp->name);
+        printf("Department: %s\n", temp->dept);
+        printf("Course: %s\n", temp->course);
+        printf("Year: %s\n", temp->year);
+        printf("Semester: %s\n", temp->semester);
+        printf("Email: %s\n", temp->email);
+        printf("Phone: %s\n", temp->phone);
+        temp = temp->next;
+    }
 }
 
-int take_data(struct Student *s)
+struct Student *create(int roll, char *name, char *dept, char *course, char *year, char *semester, char *email, char *phone)
 {
-    printf("Enter Student Information\n");
-    printf("Enter Roll: ");
-    scanf("%d", &s->roll);
-    printf("Enter Name: ");
-    scanf("%s", s->name);
-    printf("Enter Department: ");
-    scanf("%s", s->dept);
-    printf("Enter Course: ");
-    scanf("%s", s->course);
-    printf("Enter Year: ");
-    scanf("%s", s->year);
-    printf("Enter Semester: ");
-    scanf("%s", s->semester);
-    printf("Enter Email: ");
-    scanf("%s", s->email);
-    printf("Enter Phone: ");
-    scanf("%s", s->phone);
+    struct Student *s = (struct Student *)malloc(sizeof(struct Student));
+    s->roll = roll;
+    strcpy(s->name, name);
+    strcpy(s->dept, dept);
+    strcpy(s->course, course);
+    strcpy(s->year, year);
+    strcpy(s->semester, semester);
+    strcpy(s->email, email);
+    strcpy(s->phone, phone);
+    s->next = NULL;
+    return s;
 }
 
-int menu()
+void add_student(struct Student **head, struct Student *new_student)
 {
-    printf("1. Take Data\n");
-    printf("2. Display Data\n");
-    printf("3. Exit\n");
-    printf("Enter your choice: ");
+    new_student->next = *head;
+    *head = new_student;
 }
 
 int main()
 {
-    struct Student s;
-    int choice = 0;
-    while (choice != 3)
+    struct Student *head = NULL;
+    int choice, roll;
+    char name[MAX];
+    char dept[MAX];
+    char course[MAX];
+    char year[MAX];
+    char semester[MAX];
+    char email[MAX];
+    char phone[MAX];
+
+    while (1)
     {
-        menu();
+        printf("1. Add Student\n");
+        printf("2. Display Students\n");
+        printf("3. Delete Student\n");
+        printf("4. Update Student\n");
+        printf("5. Search Student\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
         scanf("%d", &choice);
         switch (choice)
         {
         case 1:
-            take_data(&s);
-            choice = 0;
+            printf("Enter Student Information\n");
+            printf("Enter Roll: ");
+            scanf("%d", &roll);
+            printf("Enter Name: ");
+            scanf("%s", name);
+            printf("Enter Department: ");
+            scanf("%s", dept);
+            printf("Enter Course: ");
+            scanf("%s", course);
+            printf("Enter Year: ");
+            scanf("%s", year);
+            printf("Enter Semester: ");
+            scanf("%s", semester);
+            printf("Enter Email: ");
+            scanf("%s", email);
+            printf("Enter Phone: ");
+            scanf("%s", phone);
+            struct Student *new_student = create(roll, name, dept, course, year, semester, email, phone);
+            add_student(&head, new_student);
             break;
         case 2:
-            display(&s);
+            display(head);
             choice = 0;
             break;
         case 3:
-            exit(0);
             break;
-        default:
-            printf("Invalid Choice\n");
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            exit(0);
         }
     }
     return 0;
